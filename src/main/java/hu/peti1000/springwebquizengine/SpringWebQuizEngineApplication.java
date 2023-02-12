1c9c7573effefae610b6e80582c50b247dc60ca5
peti1000 <petije@gmail.com>
2023-02-12 14:07:24 +0100

    Initial commit

---
+++ src/main/java/hu/peti1000/springwebquizengine/SpringWebQuizEngineApplication.java
@@ -0,0 +1,13 @@
+package hu.peti1000.springwebquizengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebQuizEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebQuizEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/peti1000/springwebquizengine/SpringWebQuizEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.peti1000.springwebquizengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebQuizEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
